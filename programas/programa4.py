d2702a5e3d16c95bbdf0d589ba60ae071b24c93a
MateoBocchiardo <104955719+MateoBocchiardo@users.noreply.github.com>
2024-04-08 21:15:31 -0300

    programa4 finished

---
+++ programas/programa4.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+def prog(texto):
+    er_p4 = r'#?#?#(.*?)\\n'
+    match = re.findall(er_p4, texto)
+
+    unique_matches = list(dict.fromkeys(match))
+
+    ret = "\n".join(f"{usuario}" for usuario in unique_matches)
+    return ret
+
+if __name__ == '__main__':
+    entrada = sys.argv[1]  # archivo entrada (param)
+    salida = sys.argv[2]   # archivo salida (param)
+    
+    f = open(entrada, 'r') # abrir archivo entrada
+    datos = f.read()       # leer archivo entrada
+    f.close()              # cerrar archivo entrada
+    
+    ret = prog(datos)      # ejecutar er
+    
+    f = open(salida, 'w')  # abrir archivo salida
+    f.write(ret)           # escribir archivo salida
+    f.close()              # cerrar archivo salida
